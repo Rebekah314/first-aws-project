d2e1868e440eb8f5fb2416cc0cc98dafeb2dfa29
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-09 10:29:18 -0500

    first commit

---
+++ src/main/java/org/launchcode/firstawsproject/FirstAwsProjectApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.firstawsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAwsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/firstawsproject/FirstAwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.firstawsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
