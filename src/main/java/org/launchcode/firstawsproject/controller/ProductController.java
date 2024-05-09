3c2b0fcd5088744dcf23a64561ca83a598f859e3
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-09 10:52:55 -0500

    wrote code for HealthController

--- src/main/java/org/launchcode/firstawsproject/controller/ProductController.java
+++ src/main/java/org/launchcode/firstawsproject/controller/ProductController.java
@@ -1,4 +1,15 @@
 package org.launchcode.firstawsproject.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> health() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
