29fef372d2662ff081a8b7a41cc3e3b11b8d4d5f
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-14 20:34:30 -0500

    preparing to make ajr file for importing into aws

--- src/test/java/org/launchcode/firstawsproject/FirstAwsProjectApplicationTests.java
+++ src/test/java/org/launchcode/firstawsproject/FirstAwsProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FirstAwsProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
