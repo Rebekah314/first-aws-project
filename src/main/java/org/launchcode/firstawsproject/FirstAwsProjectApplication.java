fb9893353bee235a85d51c55bbe061fa0aaa8d9d
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-16 21:19:46 -0500

    adding commentary about maven installation

--- src/main/java/org/launchcode/firstawsproject/FirstAwsProjectApplication.java
+++ src/main/java/org/launchcode/firstawsproject/FirstAwsProjectApplication.java
@@ -3,6 +3,8 @@ package org.launchcode.firstawsproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//installed maven 3.9.6 for making this project available to aws
+
 @SpringBootApplication
 public class FirstAwsProjectApplication {
 
